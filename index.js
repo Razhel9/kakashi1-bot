948f885726c9500f7fac27fb546d642ab4539072
Razhel9 <41942906+Razhel9@users.noreply.github.com>
2018-09-18 06:16:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client =new Discord.Client();
 
-const token ="NDg5ODc0ODY5NTA1MzU5ODky.DoJG8w.XxtU-oosRbPHn0hmnFGWFvuAXz4";
+const token ="NDg5ODc0ODY5NTA1MzU5ODky.DoJG8w.XxtU-oosRbPHn0hmnFGWFvuAXz4NDg5ODc0ODY5NTA1MzU5ODky.DoJTNA.efWPFGQnNRtvB27IE3TXMbRag30";
 
 client.on ("ready", () => {
     console.log ("ready!");
